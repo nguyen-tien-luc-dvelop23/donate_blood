4f5f4867028547aaab5b004d5ebb48bc40d23ca0
Nguyen Tien Luc <1771020440@dnu.edu.vn>
2026-03-21 23:51:20 +0700

    Align EF Core to 9.0.0 and use explicit MySQL version

--- backend/Program.cs
+++ backend/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddCors(options =>
 // Configure MySQL Database
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 35))));
 
 // Configure JWT Authentication
 builder.Services.AddAuthentication(options =>
